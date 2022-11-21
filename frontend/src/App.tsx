042db30b0f7f83c4d1488c857efe209432d79912
Juha-Matti Pennala <juha-matti.pennala@siili.com>
2022-11-21 23:05:02 +0200

    convert to typescript

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
 import Parking from "./Parking";
+
+import "./App.css";
 
 function App() {
   return (
--- frontend/src/Parking.tsx
+++ frontend/src/Parking.tsx
@@ -1,14 +1,22 @@
 import React, { useEffect, useState } from "react";
 import useFetch from "react-fetch-hook";
 
-const Parking = () => {
-  const [parkingData, setParkingData] = useState([]);
-  const { isLoading, error, data } = useFetch(
+type ParkingData = {
+  name:string
+}
+
+type ParkingFetchResponse = Array<ParkingData>
+
+function Parking() {
+  const { isLoading, error, data } = useFetch<ParkingFetchResponse>(
     "http://localhost:3001/api/parking"
   );
 
   if (isLoading) {
     return <div>loading parking data</div>;
+  }
+  if (error) {
+    return <div>{error.message}</div>
   }
 
   return <div>{data && data.map((d) => <div>{d.name}</div>)}</div>;
