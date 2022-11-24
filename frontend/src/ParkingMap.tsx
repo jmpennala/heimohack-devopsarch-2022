9a58986345772a4a197ec02761f902509b2a348d
Juha-Matti Pennala <juha-matti.pennala@siili.com>
2022-11-24 17:23:59 +0200

    circle colors

--- frontend/src/ParkingMap.tsx
+++ frontend/src/ParkingMap.tsx
@@ -28,21 +28,48 @@ type ParkingData = [
   }
 ];
 
+function getColorByDataItem(item: ParkingDataItem) {
+  // green 25-100% free
+  // yellow 10-24
+  // red 0-9
+  const percentageFree = (item.spacesAvailable / item.maxCapacity) * 100.0;
+  if (percentageFree >= 25.0) {
+    return "green";
+  } else if (percentageFree >= 10.0) {
+    return "yellow";
+  }
+
+  return "red";
+}
+
+type DataItem = {
+  coords: LatLngTuple;
+  color: string;
+};
+
 const ParkingMap = ({
   parkingData,
 }: {
   parkingData: Array<ParkingDataItem>;
 }) => {
-  const data: Array<LatLngTuple> = parkingData
+  const data: Array<DataItem> = parkingData
     ?.filter((p) => p.lat && p.lon)
-    .map((p) => [p.lat, p.lon]);
+    .map((p) => ({ coords: [p.lat, p.lon], color: getColorByDataItem(p) }));
 
   return (
     <div id="map">
       <MapContainer center={[65.012615, 25.471453]} zoom={13}>
         <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
         {data?.map((d) => (
-          <Circle center={d} pathOptions={fillBlueOptions} radius={200} />
+          <Circle
+            center={d.coords}
+            pathOptions={{
+              fillColor: d.color,
+              opacity: 1.0,
+              fillOpacity: 0.4,
+            }}
+            radius={80}
+          />
         ))}
         <Marker position={[65.012615, 25.471453]}></Marker>
       </MapContainer>
